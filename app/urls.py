7f7eb99e9b51ab7702bfedee3bc3babfd4ea7358
mateusz <matek1600@o2.pl>
2022-11-25 10:21:34 +0100

    1

---
+++ app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('calendar/', views.CalendarView.as_view(), name='calendar')
+    ]
